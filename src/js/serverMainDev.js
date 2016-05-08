f8facc8bbe38655d17e84604a6b1a2b2c5cff47e
Justin Huffaker <greyepoxy2@gmail.com>
2016-05-07 19:21:23 -0700

    Working on adding support for requesting file contents from GitHub. Going to use a seperate repro to host a versioned game.

--- src/js/serverMainDev.js
+++ src/js/serverMainDev.js
@@ -3,10 +3,43 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var https = require('https');
 var app = express();
 
 app.use(bodyParser.text())
 app.use('/assets', express.static('dist/assets'));
+
+app.get('/game/:rootpath*', function(req, res) {
+  var splitRootPath = req.params.rootpath.split('/', 1);
+  var version = splitRootPath[0];
+  //TODO: validate version is a guid
+  var requestOptions = {
+    host: 'api.github.com',
+    path: '/repos/greyepoxy/elm-tick-tack-toe/contents/src/Main.elm?ref=' + version,
+    headers: {
+      'User-Agent': 'greyepoxy/elm-GameHostServer'
+    }
+  };
+  console.log('Making request to "'+requestOptions.host +requestOptions.path+'"')
+  //Looks like I can also download the file directly using
+  // https://raw.githubusercontent.com/greyepoxy/elm-tick-tack-toe/<sha>/src/Main.elm
+  
+  https.get(requestOptions, function (githubRes) {
+    console.log('Got response: ' +githubRes.statusCode);
+    console.log('HEADERS: '+ JSON.stringify(githubRes.headers));
+    githubRes.setEncoding('utf8');
+    githubRes.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+    githubRes.on('end', function() {
+      console.log('No more data in response.')
+    });
+  }).on('error', function (e) {
+    console.log('problem with request: ' + e.message);
+  });
+  
+  res.status(200).type('html').send('No crash!');
+});
 
 var Elm = require('../ServerMain.elm');
 app.get('/*', function(req, res) {
